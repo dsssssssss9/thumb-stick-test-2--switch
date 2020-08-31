7a97de7cfe4a70fb9583297a28cc1c05e9657f3c
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-08-31 22:55:36 +0100

    using pin8 & pull down setting to detect each on/off

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
 input.onPinPressed(TouchPin.P0, function () {
+	
+})
+pins.onPulsed(DigitalPin.P8, PulseValue.Low, function () {
+    basic.showIcon(IconNames.No)
+})
+pins.onPulsed(DigitalPin.P8, PulseValue.High, function () {
     basic.showIcon(IconNames.Yes)
     basic.clearScreen()
 })
-pins.onPulsed(DigitalPin.P8, PulseValue.Low, function () {
-	
-})
-pins.onPulsed(DigitalPin.P8, PulseValue.High, function () {
-	
-})
+pins.setPull(DigitalPin.P8, PinPullMode.PullDown)
 basic.forever(function () {
 	
 })
