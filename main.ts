e5a0673852f5157814ea64855a79213bedb74db1
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-08-31 23:33:55 +0100

    adjusted x/y limits to reflect actual values

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 pins.onPulsed(DigitalPin.P8, PulseValue.Low, function () {
-    basic.clearScreen()
+	
 })
 pins.onPulsed(DigitalPin.P8, PulseValue.High, function () {
     basic.showIcon(IconNames.Yes)
@@ -8,16 +8,16 @@ pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
 basic.forever(function () {
     led.plot(pins.map(
     pins.analogReadPin(AnalogPin.P0),
-    0,
-    1023,
-    5,
+    1,
+    1019,
+    4,
     0
     ), pins.map(
     pins.analogReadPin(AnalogPin.P1),
     0,
-    1023,
+    1019,
     0,
-    5
+    4
     ))
     basic.pause(100)
     basic.clearScreen()
