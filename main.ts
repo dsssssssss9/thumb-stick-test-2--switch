a3653bd2041f75f8d227dc91c705fa482a45194f
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-08-31 22:48:20 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showIcon(IconNames.Yes)
+    basic.clearScreen()
+})
+pins.onPulsed(DigitalPin.P8, PulseValue.Low, function () {
+	
+})
+pins.onPulsed(DigitalPin.P8, PulseValue.High, function () {
+	
+})
+basic.forever(function () {
+	
+})
