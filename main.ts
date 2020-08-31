c6ed93defca470ef73ee70268f9581586961857a
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-08-31 23:11:49 +0100

    add x/y read pin0/1 display as dot on screen

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-input.onPinPressed(TouchPin.P0, function () {
-	
-})
 pins.onPulsed(DigitalPin.P8, PulseValue.Low, function () {
     basic.clearScreen()
 })
@@ -9,5 +6,19 @@ pins.onPulsed(DigitalPin.P8, PulseValue.High, function () {
 })
 pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
 basic.forever(function () {
-	
+    led.plot(pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1023,
+    5,
+    0
+    ), pins.map(
+    pins.analogReadPin(AnalogPin.P1),
+    0,
+    1023,
+    0,
+    5
+    ))
+    basic.pause(100)
+    basic.clearScreen()
 })
